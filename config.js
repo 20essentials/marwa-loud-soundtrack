68e746b7b0fcbda1390b4ddeb29549068c7f4beb
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:17:52 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MARWA LOUD';
+export let nameSongs = [
+	"BAD BOY", /*1*/
+	"BILLET", /*2*/
+	"FALLAIT PAS", /*3*/
+	"OH LA FOLLE", /*4*/
+	"QU'EST-CE QUE T'AS ?", /*5*/
+	"TELL ME", /*6*/
+	"JE VOULAIS", /*7*/
+	"GHIR NTIYA", /*8*/
+	"ALLO", /*9*/
+	"BIMBO", /*10*/
+	"ALLEZ LES GROS", /*11*/
+	"8ANS DE SALAIRE", /*12*/
+	"BABY LOVE", /*13*/
+	"MOUL TAXI", /*14*/
+	"BAH OUAIS", /*15*/
+	"T'ES OÙ ?", /*16*/
+	"MYTHO", /*17*/
+	"TU PEUX PARIER", /*18*/
+	"TEMPS PERDU", /*19*/
+	"MEHDI", /*20*/
+]
